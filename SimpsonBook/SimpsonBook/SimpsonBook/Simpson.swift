2d4560c3e3305bfd3ac9eb598cb6bb4054ed2b95
Grimhild <ufukpolat9@gmail.com>
2022-08-03 09:57:52 +0300

    Simpson Book

---
+++ SimpsonBook/SimpsonBook/SimpsonBook/Simpson.swift
@@ -0,0 +1,12 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Ufuk on 3.08.2022.
+//
+
+import Foundation
+
+class Simpson {
+    
+}
---
+++ SimpsonBook/SimpsonBook/SimpsonBook/detailsVC.swift
@@ -0,0 +1,25 @@
+//
+//  detailsVC.swift
+//  SimpsonBook
+//
+//  Created by Ufuk on 3.08.2022.
+//
+
+import UIKit
+
+class detailsVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+}

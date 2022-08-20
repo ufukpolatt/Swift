917b8ab3f8ee3fc311aa20ed531bf62f2461607d
Grimhild <ufukpolat9@gmail.com>
2022-08-20 23:50:21 +0300

    App Continues
    Travel Book

--- TravelBook/Travel Book/Travel Book/ViewController.swift
+++ TravelBook/Travel Book/Travel Book/ViewController.swift
@@ -7,18 +7,50 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         
-        
+        let gesturRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gestureRecognizer:)))
+        gesturRecognizer.minimumPressDuration = 3
+        mapView.addGestureRecognizer(gesturRecognizer)
+    
     }
 
+    @objc func chooseLocation(gestureRecognizer:UILongPressGestureRecognizer){
+        
+        if gestureRecognizer.state == .began{
+            
+            let touchedPoint = gestureRecognizer.location(in: self.mapView)
+            let touchedCoordinates = self.mapView.convert(touchedPoint, toCoordinateFrom: self.mapView)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchedCoordinates
+            annotation.title = "New Annotation"
+            annotation.subtitle = "Travel Book"
+            self.mapView.addAnnotation(annotation)
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion (center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
 
 }
 

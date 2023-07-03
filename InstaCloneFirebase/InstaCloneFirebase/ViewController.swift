a690322240152ef9668f8e20de28cc260ab7d99e
Grimhild <ufukpolat9@gmail.com>
2023-07-03 16:10:41 +0300

    Up

---
+++ InstaCloneFirebase/InstaCloneFirebase/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Ufuk on 3.07.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
